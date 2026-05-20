920446031e657146cc66e570eeb405495d695e19
Kumar Sambhav <sambhavdubey214@gmail.com>
2026-05-21 03:37:56 +0530

    fixed requirements

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     #page_icon="🎵",
     #layout="wide"
 )
-st.image("images/banner.png", width=500)
+st.image("images/banner1.png", width=500)
 st.title("Song Recommender System")
 st.write("Find song similar to your Request")
 
