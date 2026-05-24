7e56e534725c28ac6ff82d2005acb91a23b44f5c
Kumar Sambhav <sambhavdubey214@gmail.com>
2026-05-24 12:48:56 +0530

    fixed requirements

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.sidebar.title("About")
 st.sidebar.write(
     "This is a Machine Learning based Spotify song recommendation system that suggest similar song based on user selection.   "
     "The project uses KNN and Count Vectorization to find similarities and generate recommendations.\n " 
-    "The suggesions are based on the data set used, so it may not have all the songs available on spotify")
+    "The suggesions are based on the data set used to make this project, so it may not have all the songs that are available on spotify")
 
 def fetch_poster(song_name, artist_name):
     query = f"{song_name} {artist_name}"
